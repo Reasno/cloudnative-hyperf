b001efb8e422058e2b2fc3e00873a189d047db33
Reasno <guxi99@gmail.com>
2020-04-21 11:03:39 +0800

    人人爱玩

--- config/autoload/file.php
+++ config/autoload/file.php
@@ -11,14 +11,23 @@ declare(strict_types=1);
  */
 
 return [
-    'credentials' => [
-        'key' => env('S3_KEY'),
-        'secret' => env('S3_SECRET'),
-    ],
-    'region' => env('S3_REGION'),
-    'version' => 'latest',
-    'bucket_endpoint' => false,
-    'use_path_style_endpoint' => true,
-    'endpoint' => env('S3_ENDPOINT'),
-    'bucket_name' => env('S3_BUCKET'),
+    'default' => 's3',
+    'handlers' => [
+        's3' => [
+            'credentials' => [
+                'key' => env('S3_KEY'),
+                'secret' => env('S3_SECRET'),
+            ],
+            'region' => env('S3_REGION'),
+            'version' => 'latest',
+            'bucket_endpoint' => false,
+            'use_path_style_endpoint' => true,
+            'endpoint' => env('S3_ENDPOINT'),
+            'bucket_name' => env('S3_BUCKET'),
+        ],
+        'oss' => [
+
+        ]
+    ]
+
 ];
